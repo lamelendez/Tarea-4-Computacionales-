9e2fb7cf882c0cd751af3c98455fab4378152256
lamelendez <42391440+lamelendez@users.noreply.github.com>
2018-11-17 17:17:57 -0500

    Add files via upload

--- PDE.cpp
+++ PDE.cpp
@@ -3,11 +3,28 @@
 #include <cmath>
 using namespace std;
 
-int filas = 500 , columnas = 500,l=100;
+
+int filas = 500 , columnas = 500, varilla=100;
 float k=1.62,cp=820,p=2.71;
 float v=k/(cp*p);
+
 int main()
 
  return 0;
 }
 
+
+void ecuaciondifin(float seccion[][columnas])
+{
+	int contador = 0;
+        do {	
+	for (int x = 1 ; x < filas-1 ; x++)
+	{
+		for (int y = 1 ; y < columnas-1 ; y++)
+			{
+			seccion[x][y]=v*(seccion[x+1][y]+seccion[x][y+1]+seccion[x-1][y]+seccion[x][y-1]);
+			}
+	}
+        }
+	while (++contador<=N);
+}
