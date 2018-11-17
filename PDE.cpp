a4fb49d8d1342ad82adee1327848b184fad613c8
lamelendez <42391440+lamelendez@users.noreply.github.com>
2018-11-17 12:22:52 -0500

    Add files via upload

---
+++ PDE.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+
+int filas = 500 , columnas = 500,l=100;
+float k=1.62,cp=820,p=2.71;
+float v=k/(cp*p);
+int main()
+
+ return 0;
+}
+
