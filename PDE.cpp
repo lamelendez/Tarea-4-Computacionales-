de8a4c0cc20c32ef92956cf134a02a9383f72adf
lamelendez <42391440+lamelendez@users.noreply.github.com>
2018-11-18 01:10:31 -0500

    Add files via upload

--- PDE.cpp
+++ PDE.cpp
@@ -5,16 +5,20 @@ using namespace std;
 
 //inicializamos las constantes 
 
- int filas = 500,columnas = 500,r=50,h=1,N=100,centrox=250,centroy=250;  
- float k=1.62,cp=820,p=2.71,dt=0.00001;
- float v=k/(cp*p);
+ const int filas = 500,columnas = 500,r=50,h=1,N=10,centrox=250,centroy=250;
+ float k=1.62,cp=820,p=2.71,dt=9.22e-5;
+ float v=2710;
 
 void circuloV(float seccion[][columnas],int r, int centrox,int centroy);
-void condicionesFrontera(float seccion[][columnas],int caso);
+void condicionesFrontera(float seccion[][columnas],float temperatura,int caso);
 void ecuaciondifin1(float seccion[][columnas]);
+void datos(float seccion[][columnas], string nombre);
 
 int main()
 {
+ float roca[filas][columnas] = {};
+ ecuaciondifin1(roca);
+ datos(roca,"circulo.txt");
  return 0;
 }
 
@@ -26,7 +30,7 @@ void circuloV(float seccion[][columnas],int r, int centrox,int centroy)
 	{
    for(int j = centroy - r; j <= centroy + r; j++)
    	{
-       if((centrox-r)*(centrox-r) + (centroy-r)*(centroy-r) <= r*r)
+       if((i-centrox)*(i-centrox) + (j-centroy)*(j-centroy) <= r*r)
        {
          seccion[i][j]=100;
        }
@@ -35,17 +39,16 @@ void circuloV(float seccion[][columnas],int r, int centrox,int centroy)
 }
 
 //condiciones de frontera dadas por el enunciado segun los 3 casos.
-void condicionesFrontera(float seccion [][columnas], int caso)
+void condiciones(float seccion [][columnas],float temperatura, int caso)
 {
-	if (caso==1)   //condicion de frontera fija 10 grados
+	if (caso==1)
 		{
-		for (int y = 0 ; y < columnas-1 ; y++)
-			{	seccion[0][y]=10;
-				seccion[filas-1][y]=10;
-				seccion[y][0]=10;
-				seccion[y][columnas-1]=10;
-			}
-
+		for (int y = 0 ; y < columnas-1 ; y++)             //condiciones de frontera fijas
+			{	seccion[0][y]=temperatura;
+				seccion[filas-1][y]=temperatura;
+				seccion[y][0]=temperatura;
+				seccion[y][columnas-1]=temperatura;
+		         }
 		}	
 	if (caso==2)
 		{
@@ -55,29 +58,21 @@ void condicionesFrontera(float seccion [][columnas], int caso)
 				seccion[filas-1][y]=10;
 				seccion[y][0]=10;
 				seccion[y][columnas-1]=10;
-
 			}
-
-
 		}	
 	if (caso==3)
 		{
 
 		for (int y = 0 ; y < columnas-1 ; y++)
-			{
-				seccion[0][y]=10;
+			{	seccion[0][y]=10;
 				seccion[filas-1][y]=10;
 				seccion[y][0]=10;
 				seccion[y][columnas-1]=10;
-
 			}
-
 		}
-
 }
 
 //ecuacion de propagacion 
-
 void ecuaciondifin1(float seccion[][columnas])
 {
 	int contador = 0;
@@ -88,8 +83,35 @@ void ecuaciondifin1(float seccion[][columnas])
 	    for (int y = 1 ; y < columnas-1 ; y++)
 			{
                         circuloV(seccion,r,centrox,centroy);
+                        condiciones(seccion,10,1);
 			seccion[x][y]=(1-(4*dt*v)/(h*h))*seccion[x][y]+(dt*v/h*h)*(seccion[x+1][y]+seccion[x][y+1]+seccion[x-1][y]+seccion[x][y-1]);
 			}
 	}
 	while (++contador <=N);
+}
+
+//guarda los datos en un archivo txt 
+void datos(float seccion[][columnas], string nombre)
+{
+  ofstream myfile (string nombre);
+  if (myfile.is_open())
+  {
+    for(int x = 0; x < filas; x ++)
+    {
+     if(x!=0)	{
+     		myfile << "\n";
+		}    
+     	for(int y = 0; y < columnas; y ++)
+     		{  
+        	myfile << seccion[x][y] << "," ;
+ 		}   
+    }
+    myfile.close();
+  }
+  else cout << "No se pudieron guardar los datos";
+}
+
+
+
+
 }
